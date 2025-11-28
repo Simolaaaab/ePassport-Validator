2da8528b92050dfe59b8cb6bee1350069d84b44c
s297421 <simone.capriolo2002@gmail.com>
2025-11-28 11:11:22 +0100

    update main

--- src/main.py
+++ src/main.py
@@ -3,15 +3,8 @@ from validator import PassiveAuthValidator
 
 # Settimana prossima, queste funzioni useranno pyscard/pypassport
 def read_passport_simulated():
-    # MOCK: Fingiamo di aver letto il chip
-    if not os.path.exists("EF_SOD.bin"):
-        raise FileNotFoundError("Collega il lettore o fornisci file di dump!")
-    
-    with open("EF_SOD.bin", "rb") as f:
-        sod = f.read()
-    with open("EF_DG1.bin", "rb") as f:
-        dg1 = f.read()
-    return sod, dg1
+    ## da capire come farlo con pypassport!!!
+    ## NFC reader --> su ubuntu??
 
 def main():
     print("--- ePassport Passive Authentication Tool ---")
@@ -24,7 +17,7 @@ def main():
         validator = PassiveAuthValidator(sod_data, dg1_data)
         
         validator.verify_integrity()  # Hash check
-        validator.verify_signature()  # DS check
+        validator.verify_signature_with_openssl()  # DS check
         validator.verify_chain()      # CSCA check
         
         print("\n[V] PASSAPORTO VALIDO E AUTENTICO.")
